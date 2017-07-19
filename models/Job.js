dd17c65719d010f53096ff89393f662491ee7dda
Sulav Neupane <sulavneupane@gmail.com>
2017-07-19 12:46:59 -0500

    Code Refactored

---
+++ models/Job.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Sulav on 7/19/17.
+ */
+
+module.exports = class Job {
+    constructor(job) {
+        this._id = job._id;
+        this.title = job.title;
+        this.category = job.category;
+        this.preferred_date = job.preferred_date;
+        this.preferred_time = job.preferred_time;
+        this.duration = job.duration;
+        this.hourly_rate = job.hourly_rate;
+        this.description = job.description;
+        this.location = job.location;
+        this.posted_by = job.posted_by;
+        this.applied_by = job.applied_by;
+        this.status = job.status;
+    }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,19 @@
 /**
  * Created by kcp on 7/18/17.
  */
+
+var express = require('express');
+var router = express.Router();
+var User = require('../models/User');
+
+router.post('/add', function (req, res, next) {
+    let userInfo = new User(req.body.userInfo);
+
+    req.db.users.findOne({_id: userInfo._id}, function (err, dbUser) {
+        if (!err && !dbUser) {
+            req.db.users.save(userInfo);
+        }
+    });
+});
+
+module.exports = router;
