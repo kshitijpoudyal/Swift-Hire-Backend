f2d02770d1a9cccbdf7118a3795224a33e924e39
Kshitij <kcpoudyal@gmail.com>
2017-07-19 12:48:33 -0500

    user profile progress

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ app.use('/', index);
 app.use('/job', job);
 app.use('/user', user1);
 
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
--- routes/user.js
+++ routes/user.js
@@ -4,23 +4,22 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.send("hello");
-
+router.get('/job-history', function (req, res, next) {
+    let today = new Date().toISOString();
+    req.db.users.find({}).toArray(function (err, data) {
+        if (err) {
+            res.json({
+                status: 'failed',
+                message: 'Oops, something went wrong!'
+            });
+        } else {
+            res.json({
+                status: 'success',
+                user_jobs_applyed: data.jobs_applied,
+                user_jobs_posted: data.jobs_posted
+            });
+        }
+    });
 });
-// router.get('/job-history', function (req, res, next) {
-//     let today = new Date().toISOString();
-//     req.db.users.find({}).toArray(function (err, data) {
-//         if (err) {
-//             res.json({
-//                 status: 'failed',
-//                 message: 'Oops, something went wrong!'
-//             });
-//         } else {
-//             res.json({
-//                 status: 'success',
-//                 jobs: data
-//             });
-//         }
-//     });
-// });
+
+module.exports = router;
