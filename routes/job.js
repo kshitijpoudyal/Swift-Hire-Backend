306fec4b40a2ed0789c28e1acce38eb9c4f79d35
Sulav Neupane <sulavneupane@gmail.com>
2017-07-19 02:48:52 -0500

    Load profile posted posts

--- routes/job.js
+++ routes/job.js
@@ -61,6 +61,11 @@ router.post('/add', function (req, res, next) {
                     message: 'Job has been posted successfully!'
                 });
             }
+        } else {
+            res.json({
+                status: 'failed',
+                message: 'Oops, something went wrong!'
+            });
         }
     });
 });
@@ -74,18 +79,37 @@ router.get('/search', function (req, res, next) {
     });
 });
 
-router.post('/apply',function(req,res,next){
+router.post('/apply', function (req, res, next) {
     res.json(req.body);
-    
+
 });
 
-router.get('/list',function (req,res,next) {
-    let filterDate = {
-        preferred_date:{$gte:new Date()}
-    }
+router.get('/list', function (req, res, next) {
+    let today = new Date().toISOString();
 
-    req.db.jobs.find({}).toArray(function (err,data) {
-        console.log(data);
+    req.db.jobs.find({
+        'preferred_date': {$gte: today}
+    }).sort({preferred_date: 1}).limit(10).toArray(function (err, data) {
+        if (err) {
+            res.json({
+                status: 'failed',
+                message: 'Oops, something went wrong!'
+            });
+        } else {
+            res.json({
+                status: 'success',
+                jobs: data
+            });
+        }
+    });
+});
+
+router.get('/list/postedjobs/:id', function (req, res, next) {
+    let today = new Date().toISOString();
+    req.db.jobs.find({
+        'posted_by._id': req.params.id,
+        'preferred_date': {$gte: today}
+    }).sort({preferred_date: 1}).limit(10).toArray(function (err, data) {
         if (err) {
             res.json({
                 status: 'failed',
