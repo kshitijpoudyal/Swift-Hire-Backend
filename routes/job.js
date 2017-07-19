3d6f781202dae187685a548babd3e6824f6cbf1e
Kshitij <kcpoudyal@gmail.com>
2017-07-19 12:34:06 -0500

    jobs listing

--- routes/job.js
+++ routes/job.js
@@ -33,9 +33,10 @@ router.post('/add', function (req, res, next) {
                 db.jobs.save(jobInfo);
                 db.users.save(dbUser);
 
+
                 res.json({
                     status: 'success',
-                    message: 'Job has been posted successfully!'
+                    job: jobInfo
                 });
 
             } else {
@@ -58,7 +59,7 @@ router.post('/add', function (req, res, next) {
 
                 res.json({
                     status: 'success',
-                    message: 'Job has been posted successfully!'
+                    job: jobInfo
                 });
             }
         } else {
@@ -86,10 +87,9 @@ router.post('/apply', function (req, res, next) {
 
 router.get('/list', function (req, res, next) {
     let today = new Date().toISOString();
-
     req.db.jobs.find({
         'preferred_date': {$gte: today}
-    }).sort({preferred_date: 1}).limit(10).toArray(function (err, data) {
+    }).sort({preferred_date: 1}).imit(10).toArray(function (err, data) {
         if (err) {
             res.json({
                 status: 'failed',
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,26 @@
 /**
  * Created by kcp on 7/18/17.
  */
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.send("hello");
+
+});
+// router.get('/job-history', function (req, res, next) {
+//     let today = new Date().toISOString();
+//     req.db.users.find({}).toArray(function (err, data) {
+//         if (err) {
+//             res.json({
+//                 status: 'failed',
+//                 message: 'Oops, something went wrong!'
+//             });
+//         } else {
+//             res.json({
+//                 status: 'success',
+//                 jobs: data
+//             });
+//         }
+//     });
+// });
