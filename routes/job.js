789e5ba6fcb1fda4c365046fed2b9a580b5905a3
Sulav Neupane <sulavneupane@gmail.com>
2017-07-19 16:02:13 -0500

    search processing

--- routes/job.js
+++ routes/job.js
@@ -2,9 +2,26 @@
  * Created by Sulav on 7/17/17.
  */
 var express = require('express');
+var fetch = require('node-fetch');
 var router = express.Router();
 var User = require('../models/User');
 var Job = require('../models/Job');
+
+let sendSearchResult = function (req, res, searchQuery) {
+    req.db.jobs.find(searchQuery).sort({preferred_date: 1}).limit(10).toArray(function (err, data) {
+        if (err) {
+            res.json({
+                status: 'failed',
+                message: 'Oops, something went wrong!'
+            });
+        } else {
+            res.json({
+                status: 'success',
+                jobs: data
+            });
+        }
+    });
+};
 
 // api to add new job post
 router.post('/add', function (req, res, next) {
@@ -31,7 +48,7 @@ router.post('/add', function (req, res, next) {
 
                 db.jobs.save(jobInfo);
                 db.users.save(dbUser);
-              
+
                 res.json({
                     status: 'success',
                     job: jobInfo
@@ -56,16 +73,16 @@ router.get('/search', function (req, res, next) {
         searchQuery: req.query.searchQuery,
         category: req.query.category,
         location: req.query.location,
-        minFees: parseFloat(req.query.minFees)
+        minFees: parseFloat(req.query.minFees) ? parseFloat(req.query.minFees) : 0
     };
 
+    let searchQuery;
     let today = new Date().toISOString();
 
-    req.db.jobs.find({
+    searchQuery = {
         $and: [
             {'preferred_date': {$gte: today}},
             {'category': {$regex: searchParams.category, $options: 'i'}},
-            {'location.address': {$regex: searchParams.location, $options: 'i'}},
             {'hourly_rate': {$gte: searchParams.minFees}},
             {
                 $or: [{
@@ -76,19 +93,48 @@ router.get('/search', function (req, res, next) {
                 }, {'description': {$regex: searchParams.searchQuery, $options: 'i'}}]
             }
         ]
-    }).sort({preferred_date: 1}).limit(10).toArray(function (err, data) {
-        if (err) {
-            res.json({
-                status: 'failed',
-                message: 'Oops, something went wrong!'
-            });
-        } else {
-            res.json({
-                status: 'success',
-                jobs: data
-            });
-        }
-    });
+    };
+
+    if (searchParams.location !== '') {
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${searchParams.location}&key=AIzaSyA4HC3r8pJPemOT8ExqkdgSXHFdIAf19JM`).then(response => response.json()).then(data => {
+            if (data.status === 'OK') {
+                // req.db.jobs.createIndex({'location.coords': '2d'});
+                searchQuery = {
+                    $and: [
+                        {'preferred_date': {$gte: today}},
+                        {'category': {$regex: searchParams.category, $options: 'i'}},
+                        {
+                            "location.coords": {
+                                $near: {
+                                    $geometry: {
+                                        type: "Point",
+                                        coordinates: [data.results[0].geometry.location.lng, data.results[0].geometry.location.lat]
+                                    }, $maxDistance: 10000
+                                }
+                            }
+                        },
+                        {'hourly_rate': {$gte: searchParams.minFees}},
+                        {
+                            $or: [{
+                                'title': {
+                                    $regex: searchParams.searchQuery,
+                                    $options: 'i'
+                                }
+                            }, {'description': {$regex: searchParams.searchQuery, $options: 'i'}}]
+                        }
+                    ]
+                }
+            }
+            sendSearchResult(req, res, searchQuery);
+        }).catch(err => {
+            throw err
+        });
+
+    } else {
+        sendSearchResult(req, res, searchQuery);
+
+    }
+
 });
 
 router.post('/apply', function (req, res, next) {
@@ -100,7 +146,7 @@ router.get('/list', function (req, res, next) {
     let today = new Date().toISOString();
     req.db.jobs.find({
         'preferred_date': {$gte: today}
-    }).sort({preferred_date: 1}).imit(10).toArray(function (err, data) {
+    }).sort({preferred_date: 1}).limit(10).toArray(function (err, data) {
         if (err) {
             res.json({
                 status: 'failed',
