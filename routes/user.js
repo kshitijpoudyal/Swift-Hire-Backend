426a0bab8e22db2acae212c03517f65a6bd29664
Sulav Neupane <sulavneupane@gmail.com>
2017-07-20 02:19:04 -0500

    approve user

--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,12 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
+var Job = require('../models/Job');
 
 router.get('/profile/posted-jobs/:id', function (req, res, next) {
     let id = req.params.id;
 
-    req.db.users.findOne({_id:id},{jobs_posted:1},function (err, data) {
+    req.db.users.findOne({_id: id}, {jobs_posted: 1}, function (err, data) {
         if (err) {
             res.json({
                 status: 'failed',
@@ -27,7 +28,7 @@ router.get('/profile/posted-jobs/:id', function (req, res, next) {
 router.get('/profile/posted-jobs/jobs/:id', function (req, res, next) {
     req.db.jobs.findOne({
         '_id': req.params.id
-    },function (err, data) {
+    }, function (err, data) {
         if (err) {
             res.json({
                 status: 'failed',
@@ -45,7 +46,7 @@ router.get('/profile/posted-jobs/jobs/:id', function (req, res, next) {
 router.get('/profile/applied-jobs/:id', function (req, res, next) {
     let id = req.params.id;
 
-    req.db.users.findOne({_id:id},{jobs_applied:1},function (err, data) {
+    req.db.users.findOne({_id: id}, {jobs_applied: 1}, function (err, data) {
         if (err) {
             res.json({
                 status: 'failed',
@@ -63,7 +64,7 @@ router.get('/profile/applied-jobs/:id', function (req, res, next) {
 router.get('/profile/applied-jobs/jobs/:id', function (req, res, next) {
     req.db.jobs.findOne({
         '_id': req.params.id
-    },function (err, data) {
+    }, function (err, data) {
         if (err) {
             res.json({
                 status: 'failed',
@@ -85,6 +86,19 @@ router.post('/add', function (req, res, next) {
         if (!err && !dbUser) {
             req.db.users.save(userInfo);
         }
+    });
+});
+
+router.post('/approve', function (req, res, next) {
+    let userInfo = new User(req.body.userInfo);
+    let jobInfo = new Job(req.body.jobInfo);
+
+    req.db.users.save(userInfo);
+    req.db.jobs.save(jobInfo);
+
+    res.json({
+        status: 'success',
+        message: 'User has been approved successfully!'
     });
 });
 
